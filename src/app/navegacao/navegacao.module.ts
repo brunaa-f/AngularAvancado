2b6b5af448026b1f378b2ece7b604d3ae5cb3687
Bruna Fernandes <brunafernandes.idka@gmail.com>
2024-05-31 09:53:22 -0300

    to-do list

--- src/app/navegacao/navegacao.module.ts
+++ src/app/navegacao/navegacao.module.ts
@@ -1,28 +1,28 @@
 import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";   //todo component precisa desse casa, importa ele dentro do nfmodule/imports
-import { RouterModule } from "@angular/router";   //navegação
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { MenuComponent } from './menu/menu.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
-
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
-    declarations: [     //componentes desse módulo
+    declarations: [
         MenuComponent,
         HomeComponent,
         FooterComponent,
-    ], 
-    imports: [ //caso importe outros módulos
+        NotFoundComponent
+    ],
+    imports: [
         CommonModule,
         RouterModule
-    ], 
+    ],
     exports: [
-        MenuComponent,      //permitir o acesso dos components do modulo para o mundo externo
+        MenuComponent,
         HomeComponent,
         FooterComponent,
+        NotFoundComponent
     ]
 })
-export class NavegacaoModule{
-
-}
+export class NavegacaoModule{}
